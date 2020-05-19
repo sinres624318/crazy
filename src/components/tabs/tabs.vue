274b8a0dea9e7c6686cfee09d1d2d803fe17e7b8
XiaoMing <fx740068892@gmail.com>
2020-05-19 20:08:01 +0800

    '+'

--- src/components/tabs/tabs.vue
+++ src/components/tabs/tabs.vue
@@ -23,7 +23,7 @@
         });
       }
     },
-    mounted() {
+    created() {
       this.disposeNavData()
     },
     render(h) {
@@ -37,96 +37,4 @@
   }
 </script>
 
-<style lang="less">
-  .clearFix {
-    &::after {
-      content: '';
-      display: block;
-      height: 0;
-      overflow: hidden;
-      clear: both;
-    }
-  }
-
-  .crazy-tabs {
-
-    .crazy-tabs-nav__wrap {
-      user-select: none;
-      padding: 10px 24px;
-      position: relative;
-
-      &.is-scrollable {
-        padding-left: 0;
-        padding-right: 0;
-      }
-
-      .crazy-tabs-nav__container {
-        overflow: hidden;
-
-        .crazy-tabs-nav__inner {
-          white-space: nowrap;
-          float: left;
-
-          .crazy-tabs-nav-item {
-            display: inline-block;
-            white-space: nowrap;
-            margin-right: 5px;
-            background: #C0C4CC;
-            cursor: pointer;
-            padding: 12px 15px;
-
-            .crazy-tabs-nav-item__inner {
-              position: relative;
-            }
-
-            &.active {
-              background: #409EFF;
-            }
-
-            &:last-of-type {
-              margin: 0;
-            }
-          }
-        }
-      }
-
-      .crazy-tabs-nav__button {
-        position: absolute;
-        width: 24px;
-        height: 36px;
-        cursor: pointer;
-        line-height: 36px;
-        font-family: crazy, serif;
-        transform: translateY(-50%);
-        text-align: center;
-        color: #b9b9b9;
-        font-size: 24px;
-        top: 50%;
-
-        &.crazy-tabs-nav-button-disabled {
-          color: #e0e0e0;
-        }
-
-        &:hover {
-          color: #409EFF;
-
-          &.crazy-tabs-nav-button-disabled {
-            color: #e0e0e0;
-          }
-        }
-
-        &.crazy-tabs-nav-button__prev {
-          left: 0;
-        }
-
-        &.crazy-tabs-nav-button__next {
-          right: 0;
-        }
-
-        &.crazy-tabs-nav-button-disabled {
-          cursor: no-drop;
-        }
-      }
-    }
-  }
-</style>
+<style lang="less" src="./tabs.less"></style>
